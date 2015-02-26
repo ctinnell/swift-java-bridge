bf3888746e65a1d9b9aa499669931f36e2f9f5d7
Clay Tinnell <ctinnell@gaig.com>
2015-02-26 18:17:30 -0500

    Initial Commit

---
+++ JavaExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JavaExample
+//
+//  Created by Clay Tinnell on 2/26/15.
+//  Copyright (c) 2015 Clay Tinnell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JavaExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JavaExample
+//
+//  Created by Clay Tinnell on 2/26/15.
+//  Copyright (c) 2015 Clay Tinnell. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
