77f2c2eee04245ed9d7aff58c55cc8682159b61c
Clay Tinnell <ctinnell@gaig.com>
2015-03-02 07:52:20 -0500

    finding class, but jvm versions are wrong.

--- JavaExample/JavaWrapper.mm
+++ JavaExample/JavaWrapper.mm
@@ -13,13 +13,14 @@
 
 @implementation JavaWrapper
 
+JNIEnv *env;
 
 
 JNIEnv* create_vm(JavaVM ** jvm) {
-    JNIEnv *env;
     JavaVMInitArgs vm_args;
     JavaVMOption options;
-    options.optionString = "-Djava.class.path=D:\\Java Src\\TestStruct"; //Path to the java source code
+    //options.optionString = "-D."; //Path to
+    options.optionString = "-Djava.class.path=/Users/ctinnell/Development/osx/JavaExample/JavaExample"; //Path to the java source code
     vm_args.version = JNI_VERSION_1_6; //JDK version. This indicates version 1.6
     vm_args.nOptions = 1;
     vm_args.options = &options;
@@ -33,6 +34,41 @@ JNIEnv* create_vm(JavaVM ** jvm) {
     return env;
 }
 
+jclass get_bridge_class() {
+    jclass bridgeClass = NULL;
+        JavaVM *jvm;
+    //bridgeClass = env->FindClass("com/objcbridge/ObjCBridge");
+    
+        if (jvm->AttachCurrentThread((void**) &env, NULL) == JNI_OK)
+        {
+            //
+            bridgeClass = env->FindClass("com/objcbridge/ObjCBridge");
+            if (env->ExceptionCheck()) {
+                printf("\nError Finding Class\n");
+                env->ExceptionDescribe();
+            }
+            bridgeClass = env->FindClass("java.lang.Math");
+
+            if (bridgeClass != nil)
+            {
+               printf("\nFound class\n");
+
+                //(*env)->DeleteLocalRef(env, javaTestCls);
+            }
+            else
+            {
+                printf("\nDidn't Find class\n"); }
+
+            //jvm->DetachCurrentThread();
+        }
+        else
+        {
+             printf("\nJVM Not Created\n");
+        }
+    return bridgeClass;
+
+}
+
 void initialize_vm() {
     JNIEnv *env;
     JavaVM * jvm;
@@ -41,7 +77,51 @@ void initialize_vm() {
 
 -(void)initialize {
     initialize_vm();
+    jclass javaBridge = get_bridge_class();
 }
+//
+//- (void) accessJavaClass
+//{
+//    JavaVM* jvm;     JNIEnv* env;
+//    
+//    JavaVMOption options[1];
+//    
+//    options[0].optionString = "-Djava.class.path=.";
+//    
+//    JavaVMInitArgs args;
+//    args.version = JNI_VERSION_1_6;
+//    
+//    args.options = options;
+//    
+//    args.nOptions = 1;
+//    
+//    int envError = JNI_CreateJavaVM(&jvm, (void**) &env, &args);
+//    
+//    if (envError == JNI_OK)
+//        
+//    {
+//        if ((*jvm)->AttachCurrentThread(jvm, (void**) &env, NULL) == JNI_OK)
+//        {
+//            jclass javaTestCls = (*env)->FindClass(env, "JavaTest");
+//            
+//            if (javaTestCls != nil)
+//            {
+//                NSLog(@”Accessed”);
+//                
+//                (*env)->DeleteLocalRef(env, javaTestCls);
+//            }
+//            else
+//            {
+//                NSLog(@”Not Accessed”); }
+//            
+//            (jvm[0])->DetachCurrentThread(jvm);
+//        }
+//    }
+//    else
+//    {
+//        NSLog(@”JVM has not created”);
+//    }
+//}
 
 
 @end
